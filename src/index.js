dcc49baa833c6bf33c3535fb14d95827b7702f10
Student <example@example.com>
2019-12-02 12:02:31 -0800

    update front-end logic with inputs and fades.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function(){
+  let playerLetters = [];
+  $("#userInput").keyup(function() {
+    playerLetters.push($("#userInput").val());
+    let playerWord = playerLetters.join('');
 
-  
+    console.log(playerWord);
+    $(".playerShip").text("<"+playerWord+">");
+    $("#userInput").val("")
+
+
+    if (playerWord === "charAt") {
+      $(".invaderWordR").fadeToggle(1500);
+      $(".playerShipL").fadeToggle(1500);
+      $(".laserShotL").fadeToggle(200);
+      $(".laserShotR").fadeToggle(200);
+      $(".invaderWordL").fadeToggle(1500);
+      $(".playerShipR").fadeToggle(1500);
+      $(".laserShotL").fadeToggle(2000);
+      $(".laserShotR").fadeToggle(2000);
+
+      playerLetters = [];
+    }
+
+  });
+
+
 });
