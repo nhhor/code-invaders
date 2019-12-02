2bf5025cb56338e69a2a25c78dd77e7fecefde07
Student <example@example.com>
2019-12-02 11:58:30 -0800

    create object class and add generateWord()

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import { XXXXX } from './backend.js';
+import { Player } from './backend.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function(){
-
-  
+  let player = new Player ();
+  let word = player.generateWord();
+  $('#word-test').text(word);
 });
