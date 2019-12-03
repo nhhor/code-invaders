6fe4582903716d4d01ad9d7f20e106abdcbb646c
Student <example@example.com>
2019-12-03 11:14:32 -0800

    update animation timing.

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
     playerLetters.push($("#userInput").val());
     $("#userInput").val("");
     let playerCode = playerLetters.join('');
-    $(".playerShip").text(`${playerCode}`);
+    $(".playerCode").text(playerCode);
 
     for(let i = 0; i < playerCode.length; i++){
       // console.log(playerCode.length);
@@ -22,28 +22,28 @@ $(document).ready(function(){
       }
     }
 
-  console.log(playerCode);
-  if(playerCode === code){
-     $(".invaderCodeR").fadeToggle(1500);
-      // $(".playerShipL").fadeToggle(1500);
-      $(".laserShotL").fadeToggle(200);
-      $(".laserShotR").fadeToggle(200);
-      $(".invaderCodeL").fadeToggle(1500);
-      // $(".playerShipR").fadeToggle(1500);
-      $(".laserShotL").fadeToggle(2000);
-      $(".laserShotR").fadeToggle(2000);
+    console.log(playerCode);
+    if(playerCode === code){
+
+      setTimeout(function() {
+        $(".invaderCodeR").fadeToggle(200);
+        $(".invaderCodeL").fadeToggle(200);
+      }, 1000);
+
+      $(".laserShotL").toggleClass("laserShotAnimation");
+      $(".laserShotR").toggleClass("laserShotAnimation");
       $(".playerShip").toggleClass("playerShipAnimationRight");
       $(".playerShip").toggleClass("playerShipAnimationLeft");
       // $(".playerShip").removeClass("playerShipAnimationRight");
       // $(".playerShip").removeClass("playerShipAnimationLeft");
-      // setTimeout($(".playerShip").removeClass("playerShipAnimationLeft"), 4.0*1000)
-      // setTimeout(function() {
-      //   $('.playerShip').removeClass('playerShipAnimationRight playerShipAnimationLeft');
-      // }, 1000);
 
-      playerLetters = [];
-      player.score += 1;
-  }
-  console.log('Score 2: ', player.score);
+      // setTimeout(function() {
+        //     $('.playerShip').removeClass('playerShipAnimationRight playerShipAnimationLeft');
+        //   }, 1000);
+
+        playerLetters = [];
+        player.score += 1;
+      }
+      console.log('Score 2: ', player.score);
+    });
   });
-});
