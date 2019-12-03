24d5e06e39621406f3dc5745bb0858a42ccc6d59
Student <example@example.com>
2019-12-02 16:46:33 -0800

    update front end logic and add style.

--- src/index.js
+++ src/index.js
@@ -14,23 +14,34 @@ $(document).ready(function(){
     playerLetters.push($("#userInput").val());
     let playerWord = playerLetters.join('');
 
-    console.log(playerWord);
+    console.log(playerLetters);
     $(".playerShip").text(playerWord);
     $("#userInput").val("");
 
 
+    // $(".playerShip").
+
     if (playerWord === word) {
       $(".invaderWordR").fadeToggle(1500);
-      $(".playerShipL").fadeToggle(1500);
+      // $(".playerShipL").fadeToggle(1500);
       $(".laserShotL").fadeToggle(200);
       $(".laserShotR").fadeToggle(200);
       $(".invaderWordL").fadeToggle(1500);
-      $(".playerShipR").fadeToggle(1500);
+      // $(".playerShipR").fadeToggle(1500);
       $(".laserShotL").fadeToggle(2000);
       $(".laserShotR").fadeToggle(2000);
+      $(".playerShip").toggleClass("playerShipAnimationRight");
+      $(".playerShip").toggleClass("playerShipAnimationLeft");
+      // $(".playerShip").removeClass("playerShipAnimationRight");
+      // $(".playerShip").removeClass("playerShipAnimationLeft");
+      // setTimeout($(".playerShip").removeClass("playerShipAnimationLeft"), 4.0*1000)
+      // setTimeout(function() {
+      //   $('.playerShip').removeClass('playerShipAnimationRight playerShipAnimationLeft');
+      // }, 1000);
 
       playerLetters = [];
     }
+
 
   });
 });
