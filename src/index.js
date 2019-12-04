c46200fc4a879d5bea6ce342ef203a743a68fab8
Student <example@example.com>
2019-12-04 11:02:55 -0800

    clean up code.

--- src/index.js
+++ src/index.js
@@ -4,14 +4,6 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-// AUDIO TEMPLATE FOR IMPORTING AND SET-UP
-// import tick from './sounds/tick.mp3';
-// const tickSound = new Audio();
-// tickSound.src = tick;
-
-// AUDIO TEMPLATE FOR TRIGGER
-// tickSound.play();
-
 import lasercannon from './sounds/laser-cannon.mp3';
 const laserCannon = new Audio();
 laserCannon.src = lasercannon;
@@ -20,7 +12,7 @@ import aliendeathray from './sounds/alien-death-ray.mp3';
 const alienDeathRay = new Audio();
 alienDeathRay.src = aliendeathray;
 
-$(document).ready(function () {
+$(document).ready(function() {
 
   let player = new Player();
   player.turnCounter = 0;
@@ -30,7 +22,7 @@ $(document).ready(function () {
   $("#userInput").focus();
   $("#userInput").select();
 
-  $("#userInput").keyup(function () {
+  $("#userInput").keyup(function() {
 
     playerLetters.push($("#userInput").val());
     $("#userInput").val("");
@@ -47,7 +39,7 @@ $(document).ready(function () {
 
       $(".playerCode").text("");
 
-      setTimeout(function(){
+      setTimeout(function() {
         $('.invaderCode').text(code);
       }, 1000);
 
@@ -82,48 +74,41 @@ $(document).ready(function () {
     }
 
     // INCORRECT CODE CONDITION
-    function charCheck(joinedArr){
-      for (let i = 0; i < joinedArr.length; i++){
+    function charCheck(joinedArr) {
+      for (let i = 0; i < joinedArr.length; i++) {
         if (code.charAt(i) != joinedArr.charAt(i)) {
+
           playerLetters = [];
           player.health -= 1;
           player.difficulties.shift();
           code = player.generateCode();
 
-          setTimeout(function(){
+          $(".playerCode").text("");
+
+          setTimeout(function() {
             alienDeathRay.play();
-            $('.invaderCode').text(code);
+            // $('.invaderCode').text(code);
           }, 500);
 
           if (player.turnCounter % 2 === 0) {
             $(".playerShip").addClass("playerShipAnimationRight");
-            // setTimeout(function() {
-            //   $(".invaderCodeL").fadeOut(250);
-            //   $(".invaderCodeR").fadeIn(250);
-            // }, 850);
           } else {
             $(".playerShip").addClass("playerShipAnimationLeft");
-            // setTimeout(function() {
-            //   $(".invaderCodeR").fadeOut(250);
-            //   $(".invaderCodeL").fadeIn(250);
-            // }, 850);
           }
 
-          setTimeout(function(){
+          setTimeout(function() {
             $(".laserShotL").removeClass("laserShotAnimation");
             $(".playerShip").removeClass("playerShipAnimationLeft");
             $(".laserShotR").removeClass("laserShotAnimation");
             $(".playerShip").removeClass("playerShipAnimationRight");
           }, 2000);
 
-          $(".playerCode").text("");
-
-          return player.turnCounter +=2;
+          return player.turnCounter += 2;
         }
       }
     }
-    charCheck(playerCode);
 
+    charCheck(playerCode);
 
     function checkLevel() {
       if (player.score === 5) {
@@ -139,9 +124,10 @@ $(document).ready(function () {
         console.log('Difficulty 5');
       }
     }
+
     checkLevel();
 
-    function gameOver(){
+    function gameOver() {
       if(player.health === 2){
         $('#three-hearts').hide();
       } else if (player.health === 1){
@@ -153,17 +139,8 @@ $(document).ready(function () {
       }
       console.log(player.health);
     }
+
     gameOver();
 
-    console.log("TURN #: ",player.turnCounter);
-
   });
-
-  // DELAY ON STARTING ANIMATIONS
-  // setTimeout(function() {
-  //   $(".playerShipAnimationRight").css("animation-delay","0s");
-  //   $(".playerShipAnimationLeft").css("animation-delay","0s");
-  //
-  //   $(".laserShotAnimation").css("animation-delay","0s");
-  // }, 2000);
 });
