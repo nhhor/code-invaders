bc7796c471b3187b0684446958869d106bb7f013
Student <example@example.com>
2019-12-04 09:27:32 -0800

    update animation timings.

--- src/index.js
+++ src/index.js
@@ -12,6 +12,12 @@ import './styles.css';
 // AUDIO TEMPLATE FOR TRIGGER
 // tickSound.play();
 
+import lasercannon from './sounds/laser-cannon.mp3';
+const laserCannon = new Audio();
+laserCannon.src = lasercannon;
+
+
+
 
 
 $(document).ready(function () {
@@ -23,67 +29,81 @@ $(document).ready(function () {
   $("#userInput").select();
 
   $("#userInput").keyup(function () {
+
     playerLetters.push($("#userInput").val());
     $("#userInput").val("");
     let playerCode = playerLetters.join('');
     $(".playerCode").text(playerCode);
 
-
-    function charCheck(joinedArr){
-      for (let i = 0; i < joinedArr.length; i++){
-        if (code.charAt(i) != joinedArr.charAt(i)) {
-          player.difficulties.shift();
-          code = player.generateCode();
-
-          $('.invaderCode').text(code);
-          $("#userInput").val("");
-          $(".playerCode").text("");
-          setTimeout(function() {
-            $(".invaderCodeR").fadeToggle(200);
-            $(".invaderCodeL").fadeToggle(200);
-          }, 1000);
-
-          $(".laserShotL").toggleClass("laserShotAnimation");
-          $(".laserShotR").toggleClass("laserShotAnimation");
-          $(".playerShip").toggleClass("playerShipAnimationRight");
-          $(".playerShip").toggleClass("playerShipAnimationLeft");
-          $(".laserShotAnimation").css("animation-delay","0s");
-
-          playerLetters = [];
-          player.health -= 1;
-          return code;
-        }
-      }
-    }
-
-    charCheck(playerCode);
-    console.log("player.difficulties B: ",player.difficulties);
-
-    console.log(playerCode);
     if(playerCode === code){
-      setTimeout(function() {
-        $(".invaderCodeR").fadeToggle(200);
-        $(".invaderCodeL").fadeToggle(200);
-      }, 1000);
-
-      $(".laserShotL").toggleClass("laserShotAnimation");
-      $(".laserShotR").toggleClass("laserShotAnimation");
-      $(".playerShip").toggleClass("playerShipAnimationRight");
-      $(".playerShip").toggleClass("playerShipAnimationLeft");
-
       $(".laserShotAnimation").css("animation-delay","0s");
 
-      $(".playerCode").text("");
+      laserCannon.play();
 
       playerLetters = [];
       player.score += 1;
       player.checkScore(player.score);
       player.difficulties.shift();
       code = player.generateCode();
-      $('.invaderCode').text(code);
-      console.log('Score: ', player.score);
+
+      setTimeout(function(){
+        $('.invaderCode').text(code);
+      }, 1000);
+
+      setTimeout(function() {
+        $(".invaderCodeR").fadeToggle(250);
+        $(".invaderCodeL").fadeToggle(250);
+      }, 850);
+
+      $(".laserShotL").toggleClass("laserShotAnimation");
+      $(".laserShotR").toggleClass("laserShotAnimation");
+      $(".playerShip").toggleClass("playerShipAnimationRight");
+      $(".playerShip").toggleClass("playerShipAnimationLeft");
+
+
+      $(".playerCode").text("");
+
       return true;
     }
+
+    function charCheck(joinedArr){
+      for (let i = 0; i < joinedArr.length; i++){
+        if (code.charAt(i) != joinedArr.charAt(i)) {
+          $(".laserShotAnimation").css("animation-delay","-2s");
+          playerLetters = [];
+          player.health -= 1;
+          player.difficulties.shift();
+          code = player.generateCode();
+
+          setTimeout(function(){
+            $('.invaderCode').text(code);
+          }, 1000);
+
+          setTimeout(function() {
+            $(".invaderCodeR").fadeToggle(250);
+            $(".invaderCodeL").fadeToggle(250);
+          }, 850);
+
+          $(".laserShotL").toggleClass("laserShotAnimation");
+          $(".laserShotR").toggleClass("laserShotAnimation");
+
+          $(".playerShip").toggleClass("playerShipAnimationRight");
+          $(".playerShip").toggleClass("playerShipAnimationLeft");
+
+
+          // setTimeout(function(){
+          //   $(".laserShotAnimation").css("animation-delay","0s");
+          // }, 500);
+
+          $(".playerCode").text("");
+
+          return code;
+        }
+      }
+    }
+
+    charCheck(playerCode);
+    
   });
 
   // DELAY ON STARTING ANIMATIONS
