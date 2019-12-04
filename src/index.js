2576fe80885f6aa8866e79f8f14c481d199d829f
Student <example@example.com>
2019-12-03 16:12:50 -0800

    update styles and animations.

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ $(document).ready(function () {
   $('.invaderCode').text(code);
   let playerLetters = [];
 
+
   $("#userInput").keyup(function () {
     playerLetters.push($("#userInput").val());
     $("#userInput").val("");
@@ -17,55 +18,67 @@ $(document).ready(function () {
     $(".playerCode").text(playerCode);
 
 
-    function charCheck(joinedArr){      for (let i = 0; i < joinedArr.length; i++){
-      if (code.charAt(i) != joinedArr.charAt(i)) {
-        player.difficulties.shift();
-        code = player.generateCode();
+    function charCheck(joinedArr){
+      for (let i = 0; i < joinedArr.length; i++){
+        if (code.charAt(i) != joinedArr.charAt(i)) {
+          player.difficulties.shift();
+          code = player.generateCode();
 
-        $('.invaderCode').text(code);
-        $("#userInput").val("");
-        $(".playerCode").text("");
-        setTimeout(function() {
-          $(".invaderCodeR").fadeToggle(200);
-          $(".invaderCodeL").fadeToggle(200);
-        }, 1000);
+          $('.invaderCode').text(code);
+          $("#userInput").val("");
+          $(".playerCode").text("");
+          setTimeout(function() {
+            $(".invaderCodeR").fadeToggle(200);
+            $(".invaderCodeL").fadeToggle(200);
+          }, 1000);
 
-        $(".laserShotL").toggleClass("laserShotAnimation");
-        $(".laserShotR").toggleClass("laserShotAnimation");
-        $(".playerShip").toggleClass("playerShipAnimationRight");
-        $(".playerShip").toggleClass("playerShipAnimationLeft");
-
-        playerLetters = [];
-        player.health -= 1;
-        return code;
+          $(".laserShotL").toggleClass("laserShotAnimation");
+          $(".laserShotR").toggleClass("laserShotAnimation");
+          $(".playerShip").toggleClass("playerShipAnimationRight");
+          $(".playerShip").toggleClass("playerShipAnimationLeft");
+          $(".laserShotAnimation").css("animation-delay","0s");
+          playerLetters = [];
+          player.health -= 1;
+          return code;
+        }
       }
     }
-  }
-  charCheck(playerCode);
-  console.log("player.difficulties B: ",player.difficulties);
 
-  console.log(playerCode);
-  if(playerCode === code){
-    setTimeout(function() {
-      $(".invaderCodeR").fadeToggle(200);
-      $(".invaderCodeL").fadeToggle(200);
-    }, 1000);
+    charCheck(playerCode);
+    console.log("player.difficulties B: ",player.difficulties);
 
-    $(".laserShotL").toggleClass("laserShotAnimation");
-    $(".laserShotR").toggleClass("laserShotAnimation");
-    $(".playerShip").toggleClass("playerShipAnimationRight");
-    $(".playerShip").toggleClass("playerShipAnimationLeft");
+    console.log(playerCode);
+    if(playerCode === code){
+      setTimeout(function() {
+        $(".invaderCodeR").fadeToggle(200);
+        $(".invaderCodeL").fadeToggle(200);
+      }, 1000);
 
-    $(".playerCode").text("");
+      $(".laserShotL").toggleClass("laserShotAnimation");
+      $(".laserShotR").toggleClass("laserShotAnimation");
+      $(".playerShip").toggleClass("playerShipAnimationRight");
+      $(".playerShip").toggleClass("playerShipAnimationLeft");
+      $(".laserShotAnimation").css("animation-delay","0s");
 
-    playerLetters = [];
-    player.score += 1;
-    player.checkScore(player.score);
-    player.difficulties.shift();
-    code = player.generateCode();
-    $('.invaderCode').text(code);
-    console.log('Score: ', player.score);
-    return true;
-  }
-});
+      $(".playerCode").text("");
+
+      playerLetters = [];
+      player.score += 1;
+      player.checkScore(player.score);
+      player.difficulties.shift();
+      code = player.generateCode();
+      $('.invaderCode').text(code);
+      console.log('Score: ', player.score);
+      return true;
+    }
+  });
+
+  // DELAY ON STARTING ANIMATIONS
+  setTimeout(function() {
+    $(".playerShipAnimationRight").css("animation-delay","0s");
+    $(".playerShipAnimationLeft").css("animation-delay","0s");
+
+    $(".laserShotAnimation").css("animation-delay","0s");
+  }, 2000);
+
 });
